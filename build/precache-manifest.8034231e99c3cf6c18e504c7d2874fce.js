31d1c8addc12c9e89f07debb2f20bf11a8e02542
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-21 14:45:34 +0000

    feat: project read to deploy

---
+++ build/precache-manifest.8034231e99c3cf6c18e504c7d2874fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99827b6d797f9b781d6d12ea1fac13de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4dda358aff04bbe16f2",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4dda358aff04bbe16f2",
+    "url": "/static/js/main.5dca7337.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);

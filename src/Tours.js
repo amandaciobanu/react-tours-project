dacf3377ef74694ad3b79e9913ee3f98e406c996
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-21 14:25:23 +0000

    feat: fetch data from API, display tours, button read more/show less

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours}) => {
+  return (
+      <section>
+        <div className='title'>
+          <h2>Our Tours </h2>
+          <div className="underline">
+        </div>
+          <div>
+            {tours.map((tour) =>{
+              return <Tour key={tour.id} {...tour}/>
+              }
+            )}
+          </div>
+        </div>
+      </section>
+  )
 };
 
 export default Tours;

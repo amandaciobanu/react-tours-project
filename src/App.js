99f0f5ebb446b5ad540f484284c200df5eac215a
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-21 14:44:33 +0000

    feat: return all tours when page is empty

--- src/App.js
+++ src/App.js
@@ -37,6 +37,16 @@ function App() {
         </main>
         )
   }
+  if (tours.length === 0 ){
+    return (
+        <div className='title'>
+          <h2>No Tours Left</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+    )
+  }
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
